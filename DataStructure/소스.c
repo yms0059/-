38419225916902334341b94033e3ad62063b1b2f
윤명식 <yms0059@naver.com>
2018-02-17 00:12:43 +0900

    전화번호부 V2

--- DataStructure/소스.c
+++ DataStructure/소스.c
@@ -11,33 +11,85 @@ void status();
 void remove();
 
 int main() {
-	char command[BUFFER_SIZE];
+	char buffer[BUFFER_SIZE];
 	while (1) {
 		printf("$ ");
-		scanf("%s", command);
-		if (strcmp(command, "add") == 0)
+		scanf("%s", buffer);
+		if (strcmp(buffer, "read") == 0)
+			load();
+		else if (strcmp(buffer, "add") == 0)
 			add();
-		else if (strcmp(command, "find") == 0)
+		else if (strcmp(buffer, "find") == 0)
 			find();
-		else if (strcmp(command, "status") == 0)
+		else if (strcmp(buffer, "status") == 0)
 			status();
-		else if (strcmp(command, "delete") == 0)
+		else if (strcmp(buffer, "delete") == 0)
 			remove();
-		else if (strcmp(command, "exit") == 0)
+		else if (strcmp(buffer, "save") == 0)
+			save();
+		else if (strcmp(buffer, "exit") == 0)
 			break;
 	}
 	return 0;
 }
 
+
+void load() {
+	char fileName[BUFFER_SIZE]; //파일 이름입력바든ㄴ곳
+	char buf1[BUFFER_SIZE];
+	char buf2[BUFFER_SIZE];
+
+	scanf("%s", fileName); //파일 이름을 입력받는다.
+
+	FILE*fp = fopen(fileName, "r");//파일에 접근하기 위해서는 먼저 파일을 열어야 open해야한다.
+	if (fp == NULL) {
+		printf("파일오픈 실패");
+		return;
+	}
+
+	while ((fcanf(fp, "%s", buf1) != EOF)) {
+		fcanf(fp, "%s", buf2);
+		names[n] = strdup(buf1);
+		numbers[n] = strdup(buf2);
+		n++;
+	}
+	fclose(fp); //일을 마무리하면 반드시 닫아주어야한다
+}
+
+void save() {
+	int i;
+	char fileName[BUFFER_SIZE];
+	char tmp[BUFFER_SIZE];
+
+	FILE *fp = fopen(fileName, "w");
+	if (fp == NULL) {
+		printf("Open failed.\n");
+		return;
+	}
+	for (i = 0; i<n; i++) {
+		fprintf(fp, "%s %s\n", names[i], numbers[i]);
+	}
+	fclose(fp);
+}
+
+
+
 void add() {
 	char buf1[BUFFER_SIZE], buf2[BUFFER_SIZE];
 	scanf("%s", buf1);
 	scanf("%s", buf2);
-	names[n] = strdup(buf1);
-	numbers[n] = strdup(buf2);
+	int i = n - 1;
+	while (i >= 0 && strcmp(names[i], buf1) > 0) {
+		names[i + 1] = names[i];
+		numbers[i + 1] = numbers[i];
+		i--;
+	}
+	names[i + 1] = strdup(buf1);
+	numbers[i + 1] = strdup(buf2);
 	n++;
 	printf("%s was added successfully.\n", buf1);
 }
+
 
 /*char *strdup(char *s)
 {
@@ -51,14 +103,11 @@ return p;
 void find() {
 	char buf[BUFFER_SIZE];
 	scanf("%s", buf);
-	int i;
-	for (i = 0; i<n; i++) {
-		if (strcmp(buf, names[i]) == 0) {
-			printf("%s\n", numbers[i]);
-			return;
-		}
-	}
-	printf("No person named '%s' exists.\n", buf);
+	int index = search(buf);
+	if (index == -1)
+		printf("No person named '%s' exists.\n", buf);
+	else
+		printf("%s\n", numbers[index]);
 }
 
 void status() {
@@ -71,15 +120,26 @@ void status() {
 void remove() {
 	char buf[BUFFER_SIZE];
 	scanf("%s", buf);
+	int index = search(buf); /* returns -1 if not exists */
+	if (index == -1) {
+		printf("No person named '%s' exists.\n", buf);
+		return;
+	}
+	int j = index;
+	for (; j<n - 1; j++) {
+		names[j] = names[j + 1];
+		numbers[j] = numbers[j + 1];
+	}
+	n--;
+	printf("'%s' was deleted successfully. \n", buf);
+}
+
+int search(char *name) {
 	int i;
 	for (i = 0; i<n; i++) {
-		if (strcmp(buf, names[i]) == 0) {
-			names[i] = names[n - 1];
-			numbers[i] = numbers[n - 1];
-			n--;
-			printf("'%s' was deleted successfully. \n", buf);
-			return;
+		if (strcmp(name, names[i]) == 0) {
+			return i;
 		}
 	}
-	printf("No person named '%s' exists.\n", buf);
+	return -1;
 }
